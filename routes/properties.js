4f2a3a6d7f0ff3d8db7983a6b515924ead374d2e
Bradw91 <imbrad91@gmail.com>
2021-08-03 12:39:12 +0200

    Set up server & routes

---
+++ routes/properties.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const {getProperties,addProperty,deleteProperty} = require('../controllers/properties')
+
+router.route('/')
+    .get(getProperties)
+    .post(addProperty);
+
+router.route('/:id')
+    .delete(deleteProperty);
+
+module.exports = router;
